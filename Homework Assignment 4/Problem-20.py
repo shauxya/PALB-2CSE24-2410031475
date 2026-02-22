fee020a4b081a15a8e537c683fec6be4d25acb65
Shaurya Agrawal <shauryagral@gmail.com>
2026-02-22 16:47:26 +0530

    Homework Assignment 4

--- Homework Assignment 4/Problem-20.py
+++ Homework Assignment 4/Problem-20.py
@@ -1,36 +1,18 @@
 #PALB-2CSE24-2410031475
-class Solution:
-    def find3Numbers(self, arr, target):
-        arr.sort()  
-        n = len(arr)
+def isSubset(a, b):
+    freq = {}
 
-        for i in range(n - 2):
-            left = i + 1
-            right = n - 1
+    for num in a:
+        freq[num] = freq.get(num, 0) + 1
 
-            while left < right:
-                curr_sum = arr[i] + arr[left] + arr[right]
-                if curr_sum == target:
-                    return True
-                elif curr_sum < target:
-                    left += 1
-                else:
-                    right -= 1
+    for num in b:
+        if num not in freq or freq[num] == 0:
+            return False
+        freq[num] -= 1
 
-        return False
+    return True
 
 
-arr1 = [1, 4, 45, 6, 10, 8]
-target1 = 13
-
-arr2 = [1, 2, 4, 3, 6, 7]
-target2 = 10
-
-arr3 = [40, 20, 10, 3, 6, 7]
-target3 = 24
-
-sol = Solution()
-
-print(sol.find3Numbers(arr1, target1)) 
-
-print(sol.find3Numbers(arr2, target2))
+print(isSubset([11, 7, 1, 13, 21, 3, 7, 3], [11, 3, 7, 1, 7]))
+print(isSubset([1, 2, 3, 4, 4, 5, 6], [1, 2, 4]))
+print(isSubset([10, 5, 2, 23, 19], [19, 5, 3]))
